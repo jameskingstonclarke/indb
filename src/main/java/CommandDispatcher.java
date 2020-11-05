69bbdb4743a8739fac34b495b0669b5d144c0910
James Clarke <jkc200013@gmail.com>
2020-11-05 09:44:58 +0000

    boilerplate

---
+++ src/main/java/CommandDispatcher.java
@@ -0,0 +1,2 @@
+public class CommandDispatcher {
+}
---
+++ src/main/java/Information.java
@@ -0,0 +1,9 @@
+public class Information {
+    public static String NAME = "InDB";
+    public static String MAJOR = "0";
+    public static String MINOR = "1";
+
+    public static String info(){
+        return NAME+":"+MAJOR+":"+MINOR;
+    }
+}
